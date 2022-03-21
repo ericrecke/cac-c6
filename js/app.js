2068676d6d5118f2f9b77a1c8fd8fdb717d82e2d
eric recke <ericrecke@hotmail.com>
2022-03-21 19:46:23 -0300

    Added last changes form class 5

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('Started');
+import Button from './button.js';
+import Image from './image.js';
+import List from './list.js';
+import Sound from './sound.js';
+// import Sound from './sound.js';
+
+let myDiv = document.querySelector('#myApp');
+
+let myButton = new Button('Click me!');
+let myImage = new Image('/img/bg.jpg','Background','300','300');
+let myList = new List(['Izuku','Uraraka','Shoto']);
+let mySound = new Sound('/sounds/music.mp3', 'audio/mpeg');
+
+myDiv.innerHTML += myButton.render();
+myDiv.innerHTML += myImage.render();
+myDiv.innerHTML += myList.render();
+myDiv.innerHTML += mySound.render();
